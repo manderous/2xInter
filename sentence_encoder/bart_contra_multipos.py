7bb2a643a366225fc71fe00dd08e88cbcbdfa472
Nora Tang <39829624+manderous@users.noreply.github.com>
2025-05-30 20:03:21 +0800

    Update bart_contra_multipos.py

--- sentence_encoder/bart_contra_multipos.py
+++ sentence_encoder/bart_contra_multipos.py
@@ -110,10 +110,6 @@ class Bart_contra_multipos(BartPretrainedModel):
             dec_prompt_mask_ids = batch['dec_prompt_mask_ids_{}'.format(str(tem_id))]
             mask_index = batch['mask_index_{}'.format(str(tem_id))]
 
-            # dec_prompt_ids = dec_prompt_ids[:27]  # 选择第0个提示模板
-            # dec_prompt_mask_ids = dec_prompt_mask_ids[:27]  # 选择第0个提示模板
-            # mask_index = mask_index[:27]  # 选择第0个提示模板
-
             decoder_prompt_outputs = self.model.decoder(
                     input_ids=dec_prompt_ids,
                     attention_mask=dec_prompt_mask_ids,
@@ -146,64 +142,4 @@ class Bart_contra_multipos(BartPretrainedModel):
             all_logits.append(torch.squeeze(logits, 0))
 
         loss_contra = 0.0
-        # # Contrastive loss 1
-        # # 参考《6_UAI2021_Contrastive prototype learning with augmented embeddings for few-shot learning》
-        # # 原型和索引集作对比
-        # num = query_0.shape[1]  # 4
-        # num_classes = query_0.shape[0]  # 19
-        # loss_contra = 0.
-        # for i in range(num_classes):
-        #     sim_instance = torch.exp(self.sim(query_0, prototypes_0[i]))  # [19,4,768]*[768]=[19,4]
-        #     pos_ins = sim_instance[i]  # [4]
-        #     neg_ins = (sim_instance.sum(0) - pos_ins)  # [4] -> [1]
-        #     loss_contra += - torch.log(pos_ins.sum(0) / (pos_ins.sum(0) + neg_ins.sum(0)))  # [1]
-        # loss_contra = loss_contra / (num * num_classes)
-
-        # # Contrastive loss 2
-        # # 参考《8_AAAI2022_Contrastnet_A contrastive learning framework for few-shot text classification》
-        # # 相同类别的向量表示和不同类别的向量表述作对比
-        # support_inds = torch.arange(0, N).view(N, 1, 1).expand(N, K, 1).long()
-        # support_inds = Variable(support_inds, requires_grad=False).to('cuda')
-        # query_inds = torch.arange(0, N).view(N, 1, 1).expand(N, Q, 1).long()
-        # query_inds = Variable(query_inds, requires_grad=False).to('cuda')
-        # support_re = support_0.reshape(N*K, D)
-        # query_re = query_0.reshape(N*Q, D)
-        # x = [support_re, query_re]
-        # X = torch.cat(x, 0)
-        # batch_labels = torch.cat([support_inds.reshape(-1), query_inds.reshape(-1)], 0)
-        # len_ = batch_labels.size()[0]
-        #
-        # # computing similarities for each positive and negative pair
-        # s = self.similarity(X, X)
-        #
-        # # computing masks for contrastive loss
-        # mask_i = 1. - torch.from_numpy(np.identity(len_)).to(batch_labels.device)  # 所有的正负样本（分母）
-        # label_matrix = batch_labels.unsqueeze(0).repeat(len_, 1)
-        # mask_j = (batch_labels.unsqueeze(1) - label_matrix == 0).float() * mask_i  # 正样本（但不考虑样本本身）（分子）
-        # pos_num = torch.sum(mask_j, 1)
-        #
-        # # weighted NLL loss
-        # s_i = torch.clamp(torch.sum(s * mask_i, 1), min=1e-10)
-        # s_j = torch.clamp(s * mask_j, min=1e-10)
-        # log_p = torch.sum(-torch.log(s_j / s_i) * mask_j, 1) / pos_num
-        # loss_contra = torch.mean(log_p)
-
-        # # Contrastive loss 3
-        # # 参考《7_EMNLP2021_Exploring task difficulty for few-shot relation extraction》
-        # # 原型和原型作对比
-        # # select positive prototypes
-        # pos_proto_hyb = prototypes_0.unsqueeze(1)  # [18, 1, 768]
-        # # select negative prototypes
-        # neg_index = torch.zeros(N, N - 1)  # [18, 17]
-        # for i in range(N):
-        #     index_ori = [i for i in range(0, N)]
-        #     index_ori.pop(i)
-        #     neg_index[i] = torch.tensor(index_ori)
-        # neg_index = neg_index.long().view(-1).cuda()  # [18*17]
-        # neg_proto_hyb = torch.index_select(prototypes_0, dim=0, index=neg_index).view(N, N - 1, -1)  # [18, 17, 768]
-        # proto_selected = torch.cat((pos_proto_hyb, neg_proto_hyb), dim=1)  # (18, 18, 768)
-        # # 但是这样做没有anchor vector，无法计算logits_proto，参考文献使用label的名称和描述来获得anchor vector。
-        # labels_proto = torch.cat((torch.ones(N, 1), torch.zeros(N, N - 1)), dim=-1).cuda()  # (18, 18)
-        # loss_contra = self.bce_loss(logits_proto, labels_proto)
-
         return all_logits, pos_logits, loss_contra
